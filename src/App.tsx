b9dc9c1103907bb0118e1b1569118ff9a9ec4a77
Suvadip <--global>
2025-03-03 17:08:33 +0530

    initialize React + TypeScript + Vite project with basic configuration and setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div>
+			<h1>
+				Welcome to react with github actions CI-CD
+			</h1>
+			<h4>Learning CI-CD</h4>
+		</div>
+	)
+}
+
+export default App
