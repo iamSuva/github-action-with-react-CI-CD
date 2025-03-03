4e36ac6fd84c7310e9448422deac06f6dad50fca
Suvadip <--global>
2025-03-03 18:04:47 +0530

    add testing setup and update workflows for CI/CD

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+test('renders h1 and h4 elements', () => {
+  render(<App />);
+
+  // Check for <h1> tag
+  const heading1 = screen.getByRole('heading', { level: 1 });
+  expect(heading1).toHaveTextContent('Welcome to react with github actions CI-CD');
+
+  // Check for <h4> tag
+  const heading4 = screen.getByRole('heading', { level: 4 });
+  expect(heading4).toHaveTextContent('Learning CI-CD');
+});
