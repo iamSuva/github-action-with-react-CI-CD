e80d5cecb505f387a88edcf8eb8fd411821ee1eb
Suvadip <--global>
2025-03-04 11:41:39 +0530

    add sonar analysis

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 const App = () => {
 	return (
-		<div>
+		<div style={{textAlign: 'center', marginTop: '50px'}}>
 			<h1>
 				Welcome to react with github actions CI-CD
 			</h1>
 			<h4>Learning CI-CD with github actions</h4>
 			<p>this is ci-cd basics</p>
+			<h5>Added sonar code analysis</h5>
 		</div>
 	)
 }
